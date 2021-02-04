b2b544112a5918bad5a9a2013216377d10fa6b80
190030009 <76930081+190030009@users.noreply.github.com>
2021-02-04 14:08:21 +0530

    Add files via upload

---
+++ studentDisplay.java
@@ -0,0 +1,30 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class studentDisplay
+ */
+public class studentDisplay extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public studentDisplay() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		StudentBean sb=(StudentBean) request.getAttribute("studentbean");
+		PrintWriter pw=response.getWriter();
+		pw.print("Welcome"+"  "+sb.getName());
+	}
+
+}
